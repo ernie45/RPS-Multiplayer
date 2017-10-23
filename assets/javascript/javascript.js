bf14cb713bcba412954cfe99bf71721733d6a576
Ernesto Hernandez <h_ernesto@ymail.com>
2017-10-22 23:35:48 -0700

    more

--- assets/javascript/javascript.js
+++ assets/javascript/javascript.js
@@ -10,6 +10,8 @@ firebase.initializeApp(config);
 var database = firebase.database();
 //GAME OBJECT WITH FUNCTIONS TO CALL THAT ARE RELATED TO THE GAME
 var rps = {
+  firstName: "",
+  secondName: "",
   whoseTurn: "",
   oneWins: 0,
   oneLosses: 0,
@@ -22,6 +24,8 @@ var rps = {
   secondPlayer: "",
   firstChoice: "",
   secondChoice: "",
+  winner: 0,
+  chatText: "",
   //INITIALIZE PLAYERS/////////////////////////////////////////////////////////////////////////////////////////////
   createPlayer: function(name){
     if (!rps.player2Chosen){
@@ -32,12 +36,13 @@ var rps = {
           losses: 0,
           choice: ""
         });
+        rps.getName();
         rps.player2Chosen = true;
         rps.secondPlayer = name;
-        ;
-        $("#player2").html(name);
+        $("#player2").html(rps.firstName);
         rps.deployWeapons("2");
         rps.setTurn(1);
+        $("#gameProgress").html("<h1>GAME ON</h1>");
       }
       else{
         database.ref("players/one").set({
@@ -46,13 +51,21 @@ var rps = {
           losses: 0,
           choice: ""
         });
+        rps.getName();
         database.ref("ties").set(rps.ties);
         rps.player1Chosen = true;
         rps.firstPlayer = name;
-        $("#player1").html(name);
+        $("#player1").html(rps.secondName);
         rps.deployWeapons("1");
+        $("#gameProgress").html("<h1>WAITING ON PLAYER2</h1>");
       }
     }
+  },
+  getName: function(){
+    database.ref("players").on("value", function(snapshot){
+      rps.firstName = snapshot.val().one.name;
+      rps.secondName = snapshot.val().two.name;
+    });
   },
   //SET WHOSE TURN IT IS
   setTurn: function(playerNumber){
@@ -94,28 +107,33 @@ var rps = {
   },
   queryChoice: function(choice, playerString){
     database.ref("players/" + playerString + "/choice").set(choice);
+    rps.retreiveChoices();
   },
   displayChoice: function(choice, playerNumber){
     $("#player" + playerNumber + "-space").html("<h1>" + choice + "</h1>");
   },
-  checkAttack: function(){
+  retreiveChoices: function(){
     database.ref("players").on("value", function(snapshot){
       rps.firstChoice = snapshot.val().one.choice;
       rps.secondChoice = snapshot.val().two.choice;
     });
-    console.log("firstChoice: " + rps.firstChoice + " secondChoice: " + rps.secondChoice);
+  },
+  checkAttack: function(){
     if((rps.firstChoice === "rock" && rps.secondChoice === "scissors") || 
       (rps.firstChoice === "paper" && rps.secondChoice === "rock") ||
       (rps.firstChoice === "scissors" && rps.secondChoice === "paper")){
       rps.oneWins++;
       rps.twoLosses++;
+      rps.winner = 1;
     }
     else if (rps.firstChoice === rps.secondChoice){
       rps.ties++;
+      rps.winner = 0;
     }
     else{
       rps.twoWins++;
       rps.oneLosses++;
+      rps.winner = 2;
     }
     rps.queryWinsLossesTies();
   },
@@ -129,14 +147,57 @@ var rps = {
     database.ref("ties").set(rps.ties);
     database.ref("players/one/losses").set(rps.oneLosses);
     database.ref("players/two/wins").set(rps.twoWins);
+    rps.getWinsLossesTies;
+  },
+  getWinsLossesTies: function(){
+    database.ref().on("value", function(snapshot){
+      var snap = snapshot.val().players;
+      rps.oneWins = snap.one.Wins;
+      rps.oneLosses = snap.one.Losses;
+      rps.twoWins = snap.two.Wins;
+      rps.twoLosses = snap.two.Losses;
+      rps.ties = snap.ties;
+    });
+  },
+  displayProgress: function(){
+    if (rps.winner === 1){
+      $("#gameProgress").html("<h1>PLAYER1 WINS</h1>");
+    }
+    else if (rps.winner === 2){
+      $("#gameProgress").html("<h1>PLAYER2 WINS</h1>");
+    }
+    else{
+      $("#gameProgress").html("<h1>TIE!!</h1>");
+    }
+  },
+  setChat: function(text){
+    database.ref("chat").set(text);
+    rps.getChat();
+  },
+  getChat: function(){
+    database.ref("chat").on("value", function(snapshot){
+      rps.chatText = snapshot.val();
+    });
+    rps.displayChat();
+  },
+  displayChat: function(){
+    var chatDiv = $("<div>");
+    chatDiv.addClass("chatText");
+    chatDiv.html(rps.chatText);
+    $("#commentBox").append(chatDiv);
   }
-
 };
 //MAIN APP CONTENT////////////////////////////////////////////////////////////////////////////////////////////////
 $(document).ready(function(){
+  rps.getName();
   $("#begin").on("click", function(){
     var playerName = $("#name").val();
     rps.createPlayer(playerName);
+  });
+  $("#send").on("click", function(){
+    var text = $("#insult").val();
+    rps.setChat(text);
+    console.log(rps.chatText);
   });
   database.ref("turn").on("value", function(snapshot){
     var firstChoice;
@@ -149,6 +210,7 @@ $(document).ready(function(){
         rps.displayChoice(firstChoice.toUpperCase(), "1");
         $("#player1-space").off("click", ".weapons1");
         rps.setTurn(2);
+        rps.retreiveChoices();
       });
     }
     else if (rps.whoseTurn === 2){
@@ -159,6 +221,7 @@ $(document).ready(function(){
         rps.deployWeapons("1");
         rps.checkAttack();
         rps.displayWinsLossesTies();
+        rps.displayProgress();
         rps.setTurn(1);
       });
     }
